9d673527deffac510649a4e840a11b4608db57af
kyra <kyra.ma56@gmail.com>
2025-09-29 20:44:39 -0400

    header ui

--- talky-app/src/Header/Header.jsx
+++ talky-app/src/Header/Header.jsx
@@ -1,19 +1,28 @@
 import styles from "./Header.module.css";
+import talkyLogo from "../assets/talky.png";
 
 function Header() {
-    return (
-        <header className={styles.header}>
-            <h1 className={styles.title}>Talky</h1>
-            <nav className={styles.nav}>
-                <ul className={styles.navList}>
-                    <li className={styles.navItem}><a href="#" className={styles.navLink}>Home</a></li>
-                    <li className={styles.navItem}><a href="#" className={styles.navLink}>Products</a></li>
-                    <li className={styles.navItem}><a href="#" className={styles.navLink}>Login</a></li>
-                </ul>
-            </nav>
-        </header>
-    ); 
+  return (
+    <header className={styles.header}>
+      <a href="#">
+        <img src={talkyLogo} alt="Talky logo" className={styles.logo} />
+      </a>
 
+      <nav className={styles.nav}>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <a href="#" className={styles.navLink}>Home</a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="#" className={styles.navLink}>Products</a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="#" className={styles.navLink}>Login</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default Header 
+export default Header;
