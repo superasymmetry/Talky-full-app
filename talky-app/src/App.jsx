b7200dafcd1f88abd625a682b400593409c5065b
kyra <kyra.ma56@gmail.com>
2025-09-30 11:48:50 -0400

    added top padding so header doesn't cover

--- talky-app/src/App.jsx
+++ talky-app/src/App.jsx
@@ -26,7 +26,6 @@ function App() {
           <button className="search-button" onClick={handleSearch}>Search</button>
         </div>
         */}
-        <br></br><br></br><br></br>
         {/* FORCE 3 columns for lessons (will always be 3 columns) */}
         <section aria-labelledby="lessons-heading" className="mb-5 mt-10">
           <h2 id="lessons-heading" className="sr-only">Lessons</h2>
